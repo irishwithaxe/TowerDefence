5e82a79a7a7e32fa7ee81f3f464e71182105d0f2
Nikolay Ilyin <irishwithaxe@gmail.com>
2016-11-27 12:42:22 +0300

    Оформлена подписка на событие клика по тайлу

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using GameCore;
+using System;
 
 public class CharacterScript : MonoBehaviour {
    public Tile Tile { get; set; }
@@ -9,7 +10,15 @@ public class CharacterScript : MonoBehaviour {
 
    // Use this for initialization
    void Start() {
+      EventManager.Instance.OnTileClicked += EventManager_OnTileClicked;
+   }
 
+   private void EventManager_OnTileClicked(object sender, GameCore.Events.TileClickEvent gameEvent) {
+      TrySetGoal(gameEvent.tile);
+   }
+
+   private void TrySetGoal(Tile tile) {
+      
    }
 
    // Update is called once per frame
