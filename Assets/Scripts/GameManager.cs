49b4c67cf58600bb9e2f487782f12d6feee41c77
Nikolay Ilyin <irishwithaxe@gmail.com>
2016-11-27 11:01:03 +0300

    использована система событий

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
+using GameCore;
 
 public class GameManager : MonoBehaviour {
 
    // Use this for initialization
    private void Start() {
+      EventManager.Instance.OnTileClicked += EventManager_OnTileClicked;
+   }
+
+   private void EventManager_OnTileClicked(object sender, GameCore.Events.TileClickEvent gameEvent) {
+      Log.Info("clicked " + gameEvent.tile.Position.ToString());
    }
 
    // Update is called once per frame
--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -6,11 +6,35 @@ public class TileScript : MonoBehaviour {
 
    public Vector3 WorldPosition { get; set; }
 
+   private bool clickProcessing = false;
+
    // Use this for initialization
    private void Start() {
    }
 
+   // OnMouseDown is called when the user has pressed the mouse button while over the GUIElement or Collider
+   private void OnMouseDown() {
+      ClickTile();
+   }
+
    // Update is called once per frame
    private void Update() {
+   }
+   private void ClickTile() {
+      if (Input.GetMouseButtonDown(0) && !clickProcessing) try {
+            clickProcessing = true;
+            var hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider == null)
+               return;
+
+            var ts = hit.collider.GetComponent<TileScript>();
+            if (ts == null)
+               return;
+
+            ts.Tile.LeftMouseClick();
+         }
+         finally {
+            clickProcessing = false;
+         }
    }
 }
